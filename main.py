dcd59d669d26e3865d27cf49fc571aaa4e850a87
Sudhir Kumar <jakhalsudhir@gmail.com>
2023-04-09 23:32:15 +0530

    Added error handling features and optimized the code

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask  import Flask, request, render_template
-import requests, json, xmltodict
+from flask import Flask, request, render_template
+import requests
+import json
+import xmltodict
 from bs4 import BeautifulSoup
 
 
@@ -13,31 +15,45 @@ def parse_files():
         sample = ''
         status = ''
         url = request.form.get("URL")
-        r = requests.get(url)
+        try:
+            r = requests.get(url)
+            r.raise_for_status()  # Raise exception for unsuccessful requests
+        except requests.exceptions.HTTPError as e:
+            status = f"HTTP Error: {e}"
+            return render_template('index.html', status=status, url=url)
+        except requests.exceptions.RequestException as e:
+            status = f"Error: {e}"
+            return render_template('index.html', status=status, url=url)
+
         status += str(r.status_code)
-        if "text/html" in r.headers["content-type"]:    
+        content_type = r.headers.get("content-type")
+
+        if "text/html" in content_type:
             soup = BeautifulSoup(r.content, 'html.parser')
             dtype = type(soup.prettify())
             sample = soup.prettify()[:int(len(soup.prettify())/5)]
-        elif r.headers.get("content-type") == "application/xml":
-            print("The response is in XML format")
-            data = xmltodict.parse(r.content)
-            print(data)
-            dtype = 'Xml'
-            sample = data
-        else:
-            print ("non html page")
+        elif content_type == "application/xml":
             try:
-                r.json()
+                data = xmltodict.parse(r.content)
+                dtype = 'Xml'
+                sample = data
+            except xmltodict.ExpatError as e:
+                status = f"XML Parsing Error: {e}"
+                return render_template('index.html', status=status, url=url)
+        else:
+            try:
                 dic = json.loads(r.content)
                 dtype = type(dic)
                 sample = dic
+            except json.JSONDecodeError as e:
+                status = f"JSON Parsing Error: {e}"
+                return render_template('index.html', status=status, url=url)
             except:
                 status += f"\nInvalid URL. Try again with correct link. Thank you !"
-                print('Not a json format')
 
         return render_template('index.html', status=status, url=url, datatype=dtype, preview=sample)
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
